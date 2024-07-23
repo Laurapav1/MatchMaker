c1cf6dca1162654091dbc5288ebbd233b684a36e
rkthtrifork <rkth@trifork.com>
2024-07-23 14:04:02 +0200

    initial test endpoints

--- controllers/auth_controller.go
+++ controllers/auth_controller.go
@@ -1,20 +1,28 @@
 package controllers
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 // POST /signup
 func Signup(c *gin.Context) {
-
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Signup",
+	})
 }
 
 // POST /login
 func Login(c *gin.Context) {
-
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Login",
+	})
 }
 
 // GET /logout
 func Logout(c *gin.Context) {
-
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Logout",
+	})
 }
